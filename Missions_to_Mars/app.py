c94bdd5673c8c83e811b161586a49d324a50c456
Madeleine Spencer <maddie.spencer@gmail.com>
2020-11-23 03:49:18 +0800

    screenshots

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import mission_to_mars
 
+
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_app")
-# Find one record of data from the mongo database
-mission_data = mongo.db.mission_data
+
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
+    # Find one record of data from the mongo database
+    mission_data = mongo.db.mission_data
+
     # Return template and data
-    return render_template("index.html")
+    return render_template("index.html", mars=mission_data)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
